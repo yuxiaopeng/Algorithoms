f70c1c601413ad561946104c4945acb4ff591316
yuxiaopeng <yuxiaopeng@zuoyebang.com>
2020-11-18 16:38:13 +0800

    chapter 1 basic gcd

---
+++ Algorithms4/src/Test.java
@@ -0,0 +1,8 @@
+import ch1.GCD;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("1997和615的最大公约数为：" + GCD.gcd(1997, 615));
+		System.out.println("100和5的最大公约数为：" + GCD.gcd(100, 5));
+	}
+}
